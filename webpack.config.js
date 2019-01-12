3ca093b19ba7877d6b47f0713c9b2dfd2eecaa47
Vishal Sharma <vishal.k.sharma@oracle.com>
2019-01-12 17:06:53 +0530

    Setting up webpack config file to build and host project files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist' //folder from where files are served
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html' 
+        })
+    ]
 };
