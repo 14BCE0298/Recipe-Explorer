a642bca4c0d9532d920a1aeed0c54a4899b0df98
Vishal Sharma <vishal.k.sharma@oracle.com>
2019-01-13 21:33:34 +0530

    Trying api call to fetch result based on dish name

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 // Global app controller
+import axios from 'axios';
+
+async function getRecipeInfo(searchItem) {
+    const apiKey = 'd635297304a6f2579cef9ac92ead470e';
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    try{
+    const recipe = await axios(`${proxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${searchItem}`);
+    const data = recipe.data.recipes;
+    console.log(data);
+    } catch(error) {
+        alert(error);
+    } 
+}
+
+getRecipeInfo('pizza');
