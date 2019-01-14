b28e165206f1e8f875819ff690f8c650e97d216f
Vishal Sharma <vishal.k.sharma@oracle.com>
2019-01-14 18:24:28 +0530

    Exporting model class to index.js and calling api

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
 // Global app controller
-import axios from 'axios';
+import search from './models/Search'
 
-async function getRecipeInfo(searchItem) {
-    const apiKey = 'd635297304a6f2579cef9ac92ead470e';
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    try{
-    const recipe = await axios(`${proxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${searchItem}`);
-    const data = recipe.data.recipes;
-    console.log(data);
-    } catch(error) {
-        alert(error);
-    } 
-}
-
-getRecipeInfo('pizza');
+const dish = new search('pizza');
+dish.getRecepies(); 
---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export default class Search {
+    constructor(dishName) {
+        this.dishName = dishName;
+    }
+
+        async getRecepies() {
+            const apiKey = 'd635297304a6f2579cef9ac92ead470e';
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            try{
+            const recipe = await axios(`${proxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${this.dishName}`);
+            this.data = recipe.data.recipes;
+            console.log(this.data);
+            } catch(error) {
+                alert(error);
+            }  
+        }
+}
